c514535bdbfaa6e61d89a8940094c6eb3f9e2cce
Mateus Braga <teteu040506@gmail.com>
2024-11-08 21:08:47 -0300

    wip: ex6

---
+++ lista-01/ex6.c
@@ -0,0 +1,9 @@
+/**
+ * @file ex6.c
+ * @author Mateus Braga - mateusbrg
+ * @brief Escreva um programa que leia um inteiro n e imprima os n primeiros números primos.
+ *
+ * Qual a diferença deste exercício para o ex1.c?
+ */
+
+
